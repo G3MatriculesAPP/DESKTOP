96bbbbdd0ed28a0e62f106ee22443a57ba7930c6
unknown <nomengerard@gmail.com>
2021-03-19 16:02:06 +0100

    intento boton CSV

--- src/sample/controllers/DashboardController.java
+++ src/sample/controllers/DashboardController.java
@@ -2,19 +2,22 @@ package sample.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.application.Application;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+
 import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import sample.interfaces.impl.CicleImpl;
 import sample.models.Cicle;
 import sample.models.Modul;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
@@ -28,50 +31,75 @@ public class DashboardController implements Initializable {
 	@FXML
 	private Accordion acModuls;
 
+	@FXML
+	private Button bCCSV;
+
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
-		List <Cicle> patata = MocaCicles();
+		
+		CicleImpl cicleManager = new CicleImpl();
+		
+		List <Cicle> patata = cicleManager.getAllCicles();
 		ObservableList<Cicle> cicles = FXCollections.observableArrayList();
 		cicles.addAll(patata);
 		cBCicles.setItems(cicles);
-		
-		List <Modul> moniato = MocaModul();
-		
-		for(int i=0; i<moniato.size(); i++) {
-			
-			TitledPane pane1 = new TitledPane( moniato.get(i).toString() , new Label("UFs"));
-	      
 
-	        acModuls.getPanes().add(pane1);
-	        
-			
+		List <Modul> moniato = MocaModul();
+
+		for(int i=0; i<moniato.size(); i++) {
+
+			TitledPane pane1 = new TitledPane( moniato.get(i).toString() , new Label("UFs"));
+
+
+			acModuls.getPanes().add(pane1);
+
+
 		}
 		
-		
-		
+
+
+
+
+
 	}
+	
+	@FXML
+    void busc(ActionEvent event) {
+		FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Buscar csv ciclos");
+
+        // Agregar filtros para facilitar la busqueda
+        fileChooser.getExtensionFilters().addAll(
+                
+                new FileChooser.ExtensionFilter("CSV", "*.csv")
+        );
+
+       
+        
+    }
 	
 	public List<Cicle> MocaCicles() {
 		ArrayList <Cicle> patata = new ArrayList<Cicle>();
 		for(int i=0; i<10; i++) {
-			
+
 			java.util.Date fecha = new Date();
-			
-			Cicle c= new Cicle( "hola" , "nom".concat( Integer.toString(i+1)), i , fecha, 1 );
+
+			Cicle c= new Cicle( "hola" , "nom".concat( Integer.toString(i+1))," ", i , fecha, " ");
 			patata.add(c);
-			
+
+	
 		}
 		List <Cicle> Kartofen=patata; 
 		return Kartofen;
 	}
-	
+
 	public List<Modul> MocaModul(){
 		ArrayList <Modul> moniato = new ArrayList<Modul>();
 		for(int i=0; i<10; i++) {
 
 			java.util.Date fecha = new Date();
 
-			Modul c= new Modul( "hola" , "M0".concat( Integer.toString(i+1)), i , fecha );
+			Modul c= new Modul(  );
 			moniato.add(c);
 
 		}
